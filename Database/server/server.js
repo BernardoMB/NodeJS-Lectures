20a5c505777a0db5d92e6ca20ea14be742ea27c9
BernardoMB <bmondragonbrozon@gmail.com>
2020-09-24 20:05:30 -0500

    Added

--- Database/server/server.js
+++ Database/server/server.js
@@ -1,73 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const { mongoose } = require('./07-mongoose-connection');
-const { mongo } = require('mongoose');
-const { response } = require('express');
+// Require database connection
+const { mongoose } = require('./db/mongoose');
 const { ObjectID } = require('mongodb');
 
-// Mongoose models
-const Todo = mongoose.model('Todo', {
-    title: {
-        type: String,
-        required: true,
-        // Validation. See all validations here: http://mongoosejs.com/docs/validation.html
-        minlength: 1,
-        trim: true
-    },
-    text: {
-        type: String,
-        minlength: 1,
-        trim: true
-    },
-    category: {
-        type: String,
-        default: 'Uncategorized'
-    },
-    priority: {
-        type: Number,
-        default: 1,
-        validate: {
-            validator: function(value) {
-                if (1 <= value && value <= 5) {
-                    return undefined;
-                } else {
-                    throw new Error('Priority must be a value greater or equal to 1 and smaller or equal to 5');
-                }
-            },
-            message: props => `${props.value} is not a valid priority!`
-        },
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    createdAt: {
-        type: Date,
-        default: new Date()
-    },
-    updatedAt: {
-        type: Date,
-        default: new Date()
-    }
-});
-const User = mongoose.model('User', {
-    email: {
-        type: String,
-        require: true,
-        trim: true,
-        minlength: 1,
-        unique: true
-    },
-    name: {
-        type: String,
-        require: true,
-        trim: true,
-        minlength: 1
-    }
-});
+const {Todos} = require('');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Use a middleware to parse the body of the request and response
